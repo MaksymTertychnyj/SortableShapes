b2a2b1db825f9477909efa5d0355ac01b8fbd0a6
MaksymTertychnyj <maka12008@ukr.net>
2022-08-03 09:01:20 +0300

    Create abstract class Shape, override IComparable

---
+++ SortableShapes/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortableShapes
+{
+    public abstract class Shape : IComparable<Shape>
+    {
+        public double Area { get; set; }
+
+        public int CompareTo(Shape? otherShape)
+        {
+            if (otherShape == null || otherShape.Area > this.Area)
+                return -1;
+            if (otherShape.Area < this.Area)
+                return 1;
+            return 0;
+        }
+    }
+}
