22473e64778ccc2e5e612c6a72c07e6a2ed8cb6e
MaksymTertychnyj <maka12008@ukr.net>
2022-08-03 09:08:10 +0300

    verification

--- SortableShapes/Program.cs
+++ SortableShapes/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SortableShapes;
+
+var side = 1.1234D;
+var radius = 1.1234D;
+var _base = 5D;
+var height = 2D;
+
+var shapes = new List<Shape>
+{
+    new Square(side),
+    new Circle(radius),
+    new Triangle(_base, height)
+};
+
+shapes.Sort();
+
+foreach (var shape in shapes)
+{
+    Console.WriteLine($"type: {shape.GetType().Name}, area: {shape.Area}");
+}
